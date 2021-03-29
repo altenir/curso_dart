f5d8b227dc857974b62b7720c6654e1f311cc559
altenir <altenir30@gmail.com>
2021-03-29 01:45:20 -0300

    controle

--- controle/01_if_else.dart
+++ controle/01_if_else.dart
@@ -3,10 +3,14 @@ import 'dart:math';
 main() {
   var nota = Random().nextInt(11);
 
-  print("Nota selecionada foi $nota");
-  if (nota >= 7) {
+  print("Nota selecionada foi $nota.");
+  if (nota >= 9) {
+    print("Quadro de Honra!");
+  } else if (nota >= 7) {
     print("Aprovado");
-  } else {
-    print("Não foi aprovado");
+  } else if (nota >= 5) {
+    print("Recuperação");
+  } else if (nota >= 4) {
+    print("Recuperação + Trabalho");
   }
 }
---
+++ controle/02_desafio_if_else.dart
@@ -0,0 +1,8 @@
+main() {
+  var nota = 2.3;
+
+  if (nota >= 9.0) // ; não usa ; em estruturas de controle *
+  {
+    print("Parabens vc foi aprovado");
+  }
+}
